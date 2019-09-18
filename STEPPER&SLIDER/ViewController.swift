fe15a79ca6489b8ba680ac3433c28a8cf8da0b07
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-09-18 10:06:46 +0530

    Initial Commit

---
+++ STEPPER&SLIDER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STEPPER&SLIDER
+//
+//  Created by Syed.Reshma Ruksana on 18/09/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
